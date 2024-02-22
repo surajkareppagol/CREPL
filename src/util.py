95bcb66d14fd8b9ff02cc130ac20ac0ff706594f
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-02-22 18:18:05 +0530

    🌱 Use Regex for local and global statements, handle keywords

--- src/util.py
+++ src/util.py
@@ -1,7 +1,55 @@
-def read_write_file(file_name, mode, data=None):
-    with open(file_name, mode) as file:
-        if data:
-            file.write(data)
-            return
-        data = file.readlines()
-        return data
+import os
+import re
+
+from console import Terminal
+
+console = Terminal()
+
+syntax_tokens = ["if", "else", "for", "while"]
+
+function_regex = re.compile(r"[a-z]*\s[a-z]*\(.*\)+{")
+
+
+def write_template(file_global, file_functions, file_local):
+    with open("template.c", "w") as file:
+        file.write("".join(file_global))
+        file.write("".join(file_functions))
+        file.write("".join(file_local))
+
+        file.write("return 0;\n}")
+
+
+def clear_and_print():
+    console.clear()
+    console.print_panel("[bold green]CREPL[/bold green] - C Read Eval Print Loop")
+
+
+def handle_keywords(keyword):
+    if keyword == None:
+        return
+
+    if keyword in ["exit", "exit()"]:
+        if os.path.isfile("./template.c"):
+            os.unlink("./template.c")
+        console.clear()
+        exit(0)
+    elif keyword in ["clear", "clear()"]:
+        clear_and_print()
+        return "clear"
+    elif keyword in ["reset", "reset()"]:
+        return "reset"
+    elif keyword in ["show", "show()"]:
+        return "show"
+
+
+def check_scope(code):
+    for token in syntax_tokens:
+        if token in code:
+            return "l"
+
+    function_match = function_regex.match(code)
+
+    if function_match:
+        return "g"
+    else:
+        return None
